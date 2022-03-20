81a5ad5943debc2f33a5be718ff50af0f0550d02
Roman Marko <roman.marko@bonet.sk>
2022-03-20 22:37:48 +0100

    file upload

--- web/welcome/views.py
+++ web/welcome/views.py
@@ -1,7 +1,15 @@
+from distutils.command.upload import upload
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
 
 def welcome(request):
+
+    if request.method == 'POST':
+        uploaded_file = request.FILES['file']
+
+        print(uploaded_file.name)
+        print(uploaded_file.read())
+
     return render(request, 'welcome/welcome.html')
