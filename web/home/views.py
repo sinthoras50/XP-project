dd13d74ef99a350556465498760035b924342243
Roman Marko <roman.marko@bonet.sk>
2022-03-22 16:21:17 +0100

    upload suborov cez XHR

--- web/home/views.py
+++ web/home/views.py
@@ -1,15 +1,27 @@
-from distutils.command.upload import upload
+import json
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from matplotlib.font_manager import json_load 
 
 # Create your views here.
 
 def home(request):
 
-    if request.method == 'POST':
-        uploaded_file = request.FILES['file']
-
-        print(uploaded_file.name)
-        print(uploaded_file.read())
-
     return render(request, 'home/home.html')
+
+@csrf_exempt
+def upload(request):
+
+    if request.method == 'POST':
+
+
+        print(request.FILES['file'].file.getvalue().decode("utf-8"))
+
+        ret = {
+            "bu" : 1,
+            "mu" : 2,
+            "lu" : ["a", "b", "c"]
+        }
+
+        return HttpResponse(json.dumps(ret))
